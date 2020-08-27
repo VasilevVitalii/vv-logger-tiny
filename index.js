b8950671d33d497d8f8f3763f25aa9dd346c52d9
Васильев Виталий <vasilev.v.vitalii@gmail.com>
2020-08-27 09:19:18 +0300

    add check_path

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //@ts-check
+const lib_fs = require('fs-extra')
+const lib_path = require('path')
 const lib_app = require('./app.js')
 
 exports.create = create
+exports.check_path = check_path
 
 /**
  * @typedef {lib_app} app
@@ -18,4 +21,19 @@ exports.create = create
  */
 function create(path, options) {
     return new lib_app(path, options)
+}
+
+/**
+ * @param {string} path
+ * @returns {Error}
+ */
+function check_path(path) {
+    try {
+        lib_fs.ensureDirSync(path)
+        lib_fs.writeFileSync(lib_path.join(path, 'vv_logger_tiny_check_rules'), 'check rules for create files in this path', 'utf8')
+        lib_fs.unlinkSync(lib_path.join(path, 'vv_logger_tiny_check_rules'))
+        return undefined
+    } catch (error) {
+        return error
+    }
 }
